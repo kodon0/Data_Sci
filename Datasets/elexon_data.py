ffaf9a6d4ecc5a30dd1bf9cd22b5056b571ea7bb
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2020-08-20 11:54:22 +1000

    Modified

--- Datasets/elexon_data.py
+++ Datasets/elexon_data.py
@@ -5,24 +5,42 @@ Created on Thursday August  20 11:28:59 2020
 
 @author: kieranodonnell
 """
+import pandas as pd
+import numpy as np
+import time
+from datetime import datetime, timedelta
+from pandas.tseries.offsets import MonthEnd
+from elexon import ElexonRawClient
 
 class DataAccesser():
 
     '''This class takes in a user's api_key = api_key, start = start date in YYYY-mm-dd,
-    end = end date in YYYY-mm-dd and Elexon data code in XXXXXX. Generated data will have 30 min increments
+    end = end date in YYYY-mm-dd and Elexon data code in XXXXXX. Generated data will have 30 min increments.
+    Suggest using the 1st day of the month in string format.
+
+    Requires following imports:
+
+        import pandas as pd
+        import numpy as np
+        import time
+        from datetime import datetime, timedelta
+        from pandas.tseries.offsets import MonthEnd
+        from elexon import ElexonRawClient
+
+    Methods include:
+
+    .date_ranger(self)
+    .price_getter(self)
+    .dataframe_maker(self)
+
+    Need to call .price_getter before .dataframe_maker
 
     by K O'Donnell'''
-
-    import pandas as pd
-    import numpy as np
-    import time
-    from datetime import datetime, timedelta
-    from pandas.tseries.offsets import MonthEnd
-    from elexon import ElexonRawClient
 
     global df
 
     def __init__(self, api_key, start, end, elexon_code):
+
 
         self.api_key = api_key
         self.start = start
@@ -41,7 +59,7 @@ class DataAccesser():
         '''This method pulls data from Exelon. Need to use api key etc as defined previously'''
         self.prices_since_date = []
         self.api = ElexonRawClient(self.api_key)
-        # Put in start of and end of motnh (i.e 31st etc)
+        # Put in start of and end of month (i.e 31st etc)
         for date in pd.date_range(self.start, self.end, freq='MS'):
             date = self.api.request(self.elexon_code,FromSettlementDate = date.strftime("%Y-%m-%d"),
                                          ToSettlementDate = (date + MonthEnd(1)).strftime("%Y-%m-%d"))
