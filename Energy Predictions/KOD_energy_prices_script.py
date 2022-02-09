79b8912dae09d5ba88a5709a1554eaf91f179652
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2022-02-10 09:04:57 +1100

    mods

--- Energy Predictions/KOD_energy_prices_script.py
+++ Energy Predictions/KOD_energy_prices_script.py
@@ -18,28 +18,28 @@ def retrieve_time_series(api, series_ID):
     api: API that we're connected to
     series_ID: string. Name of the series that we want to pull from the EIA API
     """
-    #Retrieve Data By Series ID 
+    #Retrieve Data By Series ID
     series_search = api.data_by_series(series=series_ID)
     ##Create a pandas dataframe from the retrieved time series
     df = pd.DataFrame(series_search)
     return df
-    
+
 
 def main():
     """
     Run main script
     """
     #Create EIA API using  specific API key
-    api_key = "716c2a14953da546decd4f96c16aba8d"
+    api_key = "KEY"
     api = eia.API(api_key)
-    
+
     #Pull the electricity price data
     series_ID='ELEC.PRICE.TX-ALL.M'
     electricity_df=retrieve_time_series(api, series_ID)
     electricity_df.reset_index(level=0, inplace=True)
     #Rename the columns for easer analysis
     electricity_df.rename(columns={'index':'Date',
-            electricity_df.columns[1]:'Electricity_Price'}, 
+            electricity_df.columns[1]:'Electricity_Price'},
             inplace=True)
     print(electricity_df)
 
